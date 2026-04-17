5e40b0c9903d6b50c39c816197afd53e3a3c449b
GabrielToran <toranigabi@gmail.com>
2026-04-16 23:55:13 -0400

    Add emitTaskUpdated and emitTaskDeleted imports

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Task = require("../models/Task");
 const User = require("../models/User");
-const { emitTaskCreated } = require("../socket");
+const { emitTaskCreated, emitTaskUpdated, emitTaskDeleted } = require("../socket");
 
 const taskPopulate = [
   { path: "userId", select: "_id name email" },
